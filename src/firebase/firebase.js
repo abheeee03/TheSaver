5d4dbc63453336595570e2a9fca03fffaf16c844
Abhee <abhaynimbalkar03@gmail.com>
2024-12-08 17:13:10 +0530

    Created Project & Added Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyCTjB8Y-PeC3vScgTLGPpH-ZiwPI-wptKs",
+  authDomain: "saver-03.firebaseapp.com",
+  projectId: "saver-03",
+  storageBucket: "saver-03.firebasestorage.app",
+  messagingSenderId: "1076674280741",
+  appId: "1:1076674280741:web:149cae742c4d6994196ecc",
+  measurementId: "G-EEZYRJEZJ8"
+};
+
+const app = initializeApp(firebaseConfig);
