73f1d46f52c59d749c26896130b4c65e02dbba82
Abhee <abhaynimbalkar03@gmail.com>
2024-12-09 20:45:57 +0530

    Icon Updated

--- src/Pages/Components/Navbar.jsx
+++ src/Pages/Components/Navbar.jsx
@@ -13,7 +13,9 @@ const Navbar = () => {
     <nav className="bg-white w-full sticky top-0 border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Saver</span>
+          <span className="self-center flex text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Saver
+            </span>
         </Link>
         <button 
           onClick={toggleMenu}
